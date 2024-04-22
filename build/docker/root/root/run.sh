8932e516e655255444aba13f8f7210fd1baf9cba
Mateusz Żółtak <zozlak@zozlak.org>
2024-04-22 18:09:48 +0200

    build/docker: update to ubuntu 22.04 (noble)

--- build/docker/root/root/run.sh
+++ build/docker/root/root/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 if [ "$USER_GID" != "" ]; then
+    sed -i -e "/x:$USER_GID:/d" /etc/group
     groupmod -g $USER_GID www-data
     chgrp -R www-data /var/www
 fi
 if [ "$USER_UID" != "" ]; then
+    sed -i -e "/x:$USER_UID:/d" /etc/passwd
     usermod -u $USER_UID www-data
     chown -R www-data /var/www
 fi
@@ -21,11 +23,11 @@ if [ ! -f /var/www/html/config.yaml ]; then
 fi
 
 if [ ! -d /var/www/postgresql ]; then
-    su -l www-data -c '/usr/lib/postgresql/14/bin/initdb -D /var/www/postgresql --auth=ident -U www-data --locale en_US.UTF-8'
-    su -l www-data -c '/usr/lib/postgresql/14/bin/pg_ctl start -D /var/www/postgresql' && \
+    su -l www-data -c '/usr/lib/postgresql/16/bin/initdb -D /var/www/postgresql --auth=ident -U www-data --locale en_US.UTF-8'
+    su -l www-data -c '/usr/lib/postgresql/16/bin/pg_ctl start -D /var/www/postgresql' && \
     su -l www-data -c 'createdb -O www-data www-data' && \
     su -l www-data -c 'psql -f /var/www/html/build/db_schema.sql' && \
-    su -l www-data -c '/usr/lib/postgresql/14/bin/pg_ctl stop -D /var/www/postgresql'
+    su -l www-data -c '/usr/lib/postgresql/16/bin/pg_ctl stop -D /var/www/postgresql'
 fi
 
 /usr/bin/supervisord -c /root/supervisord.conf
