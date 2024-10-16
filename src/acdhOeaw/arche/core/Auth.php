1608f7b3f7120a89f745be72d188e9fe169b27ed
Mateusz Żółtak <zozlak@zozlak.org>
2024-10-16 16:51:21 +0200

    Auth::logout(): invalidate the auth cookie in the right way

--- src/acdhOeaw/arche/core/Auth.php
+++ src/acdhOeaw/arche/core/Auth.php
@@ -224,10 +224,10 @@ class Auth implements AuthInterface {
         unset($_SERVER['PHP_AUTH_USER'], $_SERVER['HTTP_AUTHORIZATION'], $_SERVER['AUTHORIZATION']);
         $this->controller->advertise();
 
-        if (!empty($cfg->cookie?->name)) {
-            setcookie($cfg->cookie->name, '', 1, $cfg->cookie->path ?? '/');
+        if (!empty(RC::$config->accessControl->cookie?->name)) {
+            setcookie(RC::$config->accessControl->cookie->name, '', 1, RC::$config->accessControl->cookie->path ?? '/');
         }
-        
+
         if (!empty($redirectUrl)) {
             header("Refresh: 0; url=$redirectUrl");
         }
